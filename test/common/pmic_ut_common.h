5a9fe4b439423934d86b829fd83075f67dba1e4b
Omkar Bolla <omkar.bolla@pathpartnertech.com>
2020-05-05 14:26:29 +0530

    pmic_lld: TestApp: Added RTC unity Test Application
    - Currently supported cores: mcu1_0 and mcu1_1
    
    Signed-off-by: Rahul Rawat <x1056349@ti.com>
    Signed-off-by: Omkar Bolla <omkar.bolla@pathpartnertech.com>
    Signed-off-by: Deepa Gopinath <x1060528@ti.com>

---
+++ test/common/pmic_ut_common.h
@@ -0,0 +1,173 @@
+/******************************************************************************
+ * Copyright (c) 2020 Texas Instruments Incorporated - http://www.ti.com
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *****************************************************************************/
+/**
+ *  \file   pmic_ut_common.h
+ *
+ *  \brief  Haeder file for unit test related common APIs
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <pmic.h>
+
+
+/* CSL Header files */
+#include <ti/csl/csl_types.h>
+#include <ti/csl/soc.h>
+#include <ti/csl/hw_types.h>
+#include <ti/csl/csl_timer.h>
+
+/* OSAL Header files */
+#include <ti/osal/osal.h>
+
+/* Board Header files */
+#include <ti/board/board.h>
+#include <ti/board/board_cfg.h>
+#include <ti/board/src/devices/common/common.h>
+
+/* I2C Header files */
+#include <ti/drv/i2c/I2C.h>
+#include <ti/drv/i2c/soc/I2C_soc.h>
+
+/* SPI specific headers */
+#include <ti/drv/spi/SPI.h>
+#include <ti/drv/spi/soc/SPI_soc.h>
+#include <ti/drv/spi/test/src/SPI_log.h>
+#include <ti/drv/spi/test/src/SPI_test.h>
+
+/* Timer specific headers */
+#include <ti/board/src/j721e_evm/include/board_utils.h>
+
+#if defined(UNITY_INCLUDE_CONFIG_H) && \
+           (defined(SOC_J721E) || defined(SOC_J7200))
+#include <ti/build/unit-test/Unity/src/unity.h>
+#include <ti/build/unit-test/config/unity_config.h>
+#endif
+
+/*!
+ * \brief   Slave address of PMIC-A and PMIC-B devices
+ */
+#define LEO_PMICA_SLAVE_ADDR            (0x48U)
+#define LEO_PMICA_WDG_SLAVE_ADDR        (0x12U)
+
+#define LEO_PMICB_SLAVE_ADDR            (0x4CU)
+#define LEO_PMICB_WDG_SLAVE_ADDR        (0x13U)
+
+/*!
+ * \brief   PMIC UT test status
+ */
+#define PMIC_UT_SUCCESS            (true)
+#define PMIC_UT_FAILURE            (false)
+
+#define pmic_log              (UART_printf)
+
+/*!
+ *  \brief    Define the Pmic UT test interface
+ *
+ *  \param   testFunc            Test Function
+ *  \param   testId              Test ID
+ *  \param   testDesc            Test Description
+ *
+ */
+typedef struct Pmic_Ut_Tests_s
+{
+    bool     (*testFunc)(void *);
+    int32_t  testId;
+    char     testDesc[140U];
+} Pmic_Ut_Tests_t;
+
+/*!
+ * \brief   PMIC I2C read function.
+ *
+ * \param   pmicCorehandle [IN]     PMIC Interface Handle.
+ * \param   commMode       [IN]     Communication Mode
+ * \param   slaveAddr      [IN]     I2c device slave address
+ * \param   regAddr        [OUT]    Register address to read from.
+ * \param   pBuf           [IN]     Buffer to store data
+ * \param   bufLen         [IN]     Length of data to read.
+ *
+ * \retval  PMIC_ST_SUCCESS in case of success or appropriate error code
+ *          For valid values see \ref Pmic_ErrorCodes
+ */
+int32_t test_pmic_regRead(Pmic_CoreHandle_t  *pmicCorehandle,
+                          uint8_t             instType,
+                          uint16_t            regAddr,
+                          uint8_t            *pBuf,
+                          uint8_t             bufLen);
+
+                     /*!
+ * \brief   PMIC I2C write function.
+ *
+ * \param   pmicCorehandle   [IN]     PMIC Interface Handle.
+ * \param   commMode         [IN]     Communication Mode
+ * \param   slaveAddr        [IN]     I2c device slave address
+ * \param   regAddr          [IN]     Register address to write.
+ * \param   pBuf             [IN]     Buffer to store data to write
+ * \param   bufLen           [IN]     Length of data to write.
+ *
+ * \retval  PMIC_ST_SUCCESS in case of success or appropriate error code
+ *          For valid values see \ref Pmic_ErrorCodes
+ */
+int32_t test_pmic_regWrite(Pmic_CoreHandle_t  *pmicCorehandle,
+                           uint8_t             instType,
+                           uint16_t            regAddr,
+                           uint8_t            *pBuf,
+                           uint8_t             bufLen);
+
+/*!
+ * \brief   PMIC Critical section Lock function
+ *          This function should have OS specific locking setup and should
+ *          assigned to 'pmicConfigData.pFnPmicCritSecStart'
+ */
+void test_pmic_criticalSectionStartFn(void);
+
+/*!
+ * \brief   PMIC Critical section Unlock function
+ *          This function should have OS specific locking setup and should
+ *          assigned to 'pmicConfigData.pFnPmicCritSecStop'
+ */
+void test_pmic_criticalSectionStopFn(void);
+
+/*!
+ * \brief   Pmic Unity Tests Common function
+ *
+ * \retval  PMIC_UT_SUCCESS in case of success or
+ *          PMIC_UT_FAILURE in case of failure.
+ */
+bool test_pmic_common(Pmic_Ut_Tests_t *pmic_Test, Pmic_CoreCfg_t *pmicCfgData);
+
+/*!
+ * \brief   Configures UART pinmux and initialization for the UART Prints
+ */
+void test_pmic_uartInit();
