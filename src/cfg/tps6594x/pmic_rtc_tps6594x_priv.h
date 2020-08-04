069efc076c87c3702384f1d8ff1b7c676f813c8b
Mayank kumar <x1063644@ti.com>
2020-08-04 19:24:22 +0530

    pmic_lld: misrac fixes rule-R.10.1
    misrac fixes rule-R.10.1 - MISRA.ETYPE.INAPPR.OPERAND.BINOP.2012
    Operand(s) to a binary operator have inappropriate essential type(s) signed char, unsigned char
    
    Signed-off-by: Mayank kumar <x1063644@ti.com>

--- src/cfg/tps6594x/pmic_rtc_tps6594x_priv.h
+++ src/cfg/tps6594x/pmic_rtc_tps6594x_priv.h
@@ -170,160 +170,160 @@ extern "C" {
 /*!
  * \brief   PMIC RTC and alarm time and date bit mask
  */
-#define PMIC_RTC_SECONDS_SECOND_1_MASK       (uint8_t)(0x07<<        \
+#define PMIC_RTC_SECONDS_SECOND_1_MASK       (uint8_t)(0x07U <<        \
                                                PMIC_RTC_SECONDS_SECOND_1_SHIFT)
 
-#define PMIC_RTC_SECONDS_SECOND_0_MASK       (uint8_t)(0x0F<<        \
+#define PMIC_RTC_SECONDS_SECOND_0_MASK       (uint8_t)(0x0FU <<        \
                                                PMIC_RTC_SECONDS_SECOND_0_SHIFT)
 
-#define PMIC_RTC_MINUTES_MINUTE_1_MASK       (uint8_t)(0x07<<        \
+#define PMIC_RTC_MINUTES_MINUTE_1_MASK       (uint8_t)(0x07U <<        \
                                                PMIC_RTC_MINUTES_MINUTE_1_SHIFT)
-#define PMIC_RTC_MINUTES_MINUTE_0_MASK       (uint8_t)(0x0F<<        \
+#define PMIC_RTC_MINUTES_MINUTE_0_MASK       (uint8_t)(0x0FU <<        \
                                                PMIC_RTC_MINUTES_MINUTE_0_SHIFT)
 
-#define PMIC_RTC_HOURS_PM_NAM_MASK           (uint8_t)(0x01<<        \
+#define PMIC_RTC_HOURS_PM_NAM_MASK           (uint8_t)(0x01U <<        \
                                                    PMIC_RTC_HOURS_PM_NAM_SHIFT)
 
-#define PMIC_RTC_HOURS_HOUR_1_MASK           (uint8_t)(0x03<<        \
+#define PMIC_RTC_HOURS_HOUR_1_MASK           (uint8_t)(0x03U <<        \
                                                    PMIC_RTC_HOURS_HOUR_1_SHIFT)
 
-#define PMIC_RTC_HOURS_HOUR_0_MASK           (uint8_t)(0x0F<<        \
+#define PMIC_RTC_HOURS_HOUR_0_MASK           (uint8_t)(0x0FU <<        \
                                                    PMIC_RTC_HOURS_HOUR_0_SHIFT)
 
-#define PMIC_RTC_DAYS_DAY_1_MASK             (uint8_t)(0x03<<        \
+#define PMIC_RTC_DAYS_DAY_1_MASK             (uint8_t)(0x03U <<        \
                                                    PMIC_RTC_DAYS_DAY_1_SHIFT)
 
-#define PMIC_RTC_DAYS_DAY_0_MASK             (uint8_t)(0x0F<<        \
+#define PMIC_RTC_DAYS_DAY_0_MASK             (uint8_t)(0x0FU <<        \
                                                    PMIC_RTC_DAYS_DAY_0_SHIFT)
 
-#define PMIC_RTC_MONTHS_MONTH_1_MASK         (uint8_t)(0x01<<        \
+#define PMIC_RTC_MONTHS_MONTH_1_MASK         (uint8_t)(0x01U <<        \
                                                  PMIC_RTC_MONTHS_MONTH_1_SHIFT)
 
-#define PMIC_RTC_MONTHS_MONTH_0_MASK         (uint8_t)(0x0F<<        \
+#define PMIC_RTC_MONTHS_MONTH_0_MASK         (uint8_t)(0x0FU <<        \
                                                  PMIC_RTC_MONTHS_MONTH_0_SHIFT)
 
-#define PMIC_RTC_YEARS_YEAR_1_MASK           (uint8_t)(0x0F<<        \
+#define PMIC_RTC_YEARS_YEAR_1_MASK           (uint8_t)(0x0FU <<        \
                                                    PMIC_RTC_YEARS_YEAR_1_SHIFT)
 
-#define PMIC_RTC_YEARS_YEAR_0_MASK           (uint8_t)(0x0F<<        \
+#define PMIC_RTC_YEARS_YEAR_0_MASK           (uint8_t)(0x0FU <<        \
                                                    PMIC_RTC_YEARS_YEAR_0_SHIFT)
 
-#define PMIC_ALARM_SECONDS_ALR_SECOND_1_MASK          (uint8_t)(0x07<<        \
+#define PMIC_ALARM_SECONDS_ALR_SECOND_1_MASK          (uint8_t)(0x07U <<      \
                                          PMIC_ALARM_SECONDS_ALR_SECOND_1_SHIFT)
 
-#define PMIC_ALARM_SECONDS_ALR_SECOND_0_MASK          (uint8_t)(0x0F<<        \
+#define PMIC_ALARM_SECONDS_ALR_SECOND_0_MASK          (uint8_t)(0x0FU <<      \
                                          PMIC_ALARM_SECONDS_ALR_SECOND_0_SHIFT)
 
-#define PMIC_ALARM_MINUTES_ALR_MINUTE_1_MASK          (uint8_t)(0x07<<        \
+#define PMIC_ALARM_MINUTES_ALR_MINUTE_1_MASK          (uint8_t)(0x07U <<      \
                                          PMIC_ALARM_MINUTES_ALR_MINUTE_1_SHIFT)
-#define PMIC_ALARM_MINUTES_ALR_MINUTE_0_MASK          (uint8_t)(0x0F<<        \
+#define PMIC_ALARM_MINUTES_ALR_MINUTE_0_MASK          (uint8_t)(0x0FU <<      \
                                          PMIC_ALARM_MINUTES_ALR_MINUTE_0_SHIFT)
 
-#define PMIC_ALARM_HOURS_ALR_PM_NAM_MASK              (uint8_t)(0x01<<        \
+#define PMIC_ALARM_HOURS_ALR_PM_NAM_MASK              (uint8_t)(0x01U <<      \
                                              PMIC_ALARM_HOURS_ALR_PM_NAM_SHIFT)
 
-#define PMIC_ALARM_HOURS_ALR_HOUR_1_MASK              (uint8_t)(0x03<<        \
+#define PMIC_ALARM_HOURS_ALR_HOUR_1_MASK              (uint8_t)(0x03U <<      \
                                              PMIC_ALARM_HOURS_ALR_HOUR_1_SHIFT)
 
-#define PMIC_ALARM_HOURS_ALR_HOUR_0_MASK              (uint8_t)(0x0F<<        \
+#define PMIC_ALARM_HOURS_ALR_HOUR_0_MASK              (uint8_t)(0x0FU <<      \
                                          PMIC_ALARM_HOURS_ALR_HOUR_0_SHIFT)
 
-#define PMIC_ALARM_DAYS_ALR_DAY_1_MASK                (uint8_t)(0x03<<        \
+#define PMIC_ALARM_DAYS_ALR_DAY_1_MASK                (uint8_t)(0x03U <<      \
                                                PMIC_ALARM_DAYS_ALR_DAY_1_SHIFT)
 
-#define PMIC_ALARM_DAYS_ALR_DAY_0_MASK                (uint8_t)(0x0F<<        \
+#define PMIC_ALARM_DAYS_ALR_DAY_0_MASK                (uint8_t)(0x0FU <<      \
                                          PMIC_ALARM_DAYS_ALR_DAY_0_SHIFT)
 
-#define PMIC_ALARM_MONTHS_ALR_MONTH_1_MASK            (uint8_t)(0x01<<        \
+#define PMIC_ALARM_MONTHS_ALR_MONTH_1_MASK            (uint8_t)(0x01U <<      \
                                            PMIC_ALARM_MONTHS_ALR_MONTH_1_SHIFT)
 
-#define PMIC_ALARM_MONTHS_ALR_MONTH_0_MASK            (uint8_t)(0x0F<<        \
+#define PMIC_ALARM_MONTHS_ALR_MONTH_0_MASK            (uint8_t)(0x0FU <<      \
                                            PMIC_ALARM_MONTHS_ALR_MONTH_0_SHIFT)
 
-#define PMIC_ALARM_YEARS_ALR_YEAR_1_MASK              (uint8_t)(0x0F<<        \
+#define PMIC_ALARM_YEARS_ALR_YEAR_1_MASK              (uint8_t)(0x0FU <<      \
                                              PMIC_ALARM_YEARS_ALR_YEAR_1_SHIFT)
 
-#define PMIC_ALARM_YEARS_ALR_YEAR_0_MASK              (uint8_t)(0x0F<<        \
+#define PMIC_ALARM_YEARS_ALR_YEAR_0_MASK              (uint8_t)(0x0FU <<      \
                                              PMIC_ALARM_YEARS_ALR_YEAR_0_SHIFT)
 
-#define PMIC_RTC_WEEKS_WEEK_MASK                      (uint8_t)(0x07<<        \
+#define PMIC_RTC_WEEKS_WEEK_MASK                      (uint8_t)(0x07U <<      \
                                                      PMIC_RTC_WEEKS_WEEK_SHIFT)
 
-#define PMIC_RTC_STATUS_ALARM_MASK                    (uint8_t)(0x01<<        \
+#define PMIC_RTC_STATUS_ALARM_MASK                    (uint8_t)(0x01U <<      \
                                                    PMIC_RTC_STATUS_ALARM_SHIFT)
 
-#define PMIC_RTC_STATUS_TIMER_MASK                    (uint8_t)(0x01<<        \
+#define PMIC_RTC_STATUS_TIMER_MASK                    (uint8_t)(0x01U <<      \
                                                    PMIC_RTC_STATUS_TIMER_SHIFT)
 
-#define PMIC_RTC_STATUS_RUN_MASK                      (uint8_t)(0x01<<        \
+#define PMIC_RTC_STATUS_RUN_MASK                      (uint8_t)(0x01U <<      \
                                                      PMIC_RTC_STATUS_RUN_SHIFT)
 
-#define PMIC_RTC_STATUS_POWER_UP_MASK                 (uint8_t)(0x01<<        \
+#define PMIC_RTC_STATUS_POWER_UP_MASK                 (uint8_t)(0x01U <<      \
                                                 PMIC_RTC_STATUS_POWER_UP_SHIFT)
 
 /*!
  * \brief   PMIC RTC CTRL bit mask
  */
-#define PMIC_RTC_CTRL_1_RTC_V_OPT_MASK                (uint8_t)(0x01<<        \
+#define PMIC_RTC_CTRL_1_RTC_V_OPT_MASK                (uint8_t)(0x01U <<      \
                                                PMIC_RTC_CTRL_1_RTC_V_OPT_SHIFT)
 
-#define PMIC_RTC_CTRL_1_GET_TIME_MASK                 (uint8_t)(0x01<<        \
+#define PMIC_RTC_CTRL_1_GET_TIME_MASK                 (uint8_t)(0x01U <<      \
                                                 PMIC_RTC_CTRL_1_GET_TIME_SHIFT)
 
-#define PMIC_RTC_CTRL_1_SET_32_COUNTER_MASK           (uint8_t)(0x01<<        \
+#define PMIC_RTC_CTRL_1_SET_32_COUNTER_MASK           (uint8_t)(0x01U <<      \
                                           PMIC_RTC_CTRL_1_SET_32_COUNTER_SHIFT)
 
-#define PMIC_RTC_CTRL_1_MODE_12_24_MASK               (uint8_t)(0x01<<        \
+#define PMIC_RTC_CTRL_1_MODE_12_24_MASK               (uint8_t)(0x01U <<      \
                                               PMIC_RTC_CTRL_1_MODE_12_24_SHIFT)
 
-#define PMIC_RTC_CTRL_1_ROUND_30S_MASK                (uint8_t)(0x01<<        \
+#define PMIC_RTC_CTRL_1_ROUND_30S_MASK                (uint8_t)(0x01U <<      \
                                                PMIC_RTC_CTRL_1_ROUND_30S_SHIFT)
 
-#define PMIC_RTC_CTRL_1_STOP_RTC_MASK                 (uint8_t)(0x01<<        \
+#define PMIC_RTC_CTRL_1_STOP_RTC_MASK                 (uint8_t)(0x01U <<      \
                                                 PMIC_RTC_CTRL_1_STOP_RTC_SHIFT)
 
-#define PMIC_RTC_CTRL_1_AUTO_COMP_MASK                (uint8_t)(0x01<<        \
+#define PMIC_RTC_CTRL_1_AUTO_COMP_MASK                (uint8_t)(0x01U <<      \
                                                PMIC_RTC_CTRL_1_AUTO_COMP_SHIFT)
 
-#define PMIC_RTC_CTRL_2_FIRST_STARTUP_DONE_MASK       (uint8_t)(0x01<<        \
+#define PMIC_RTC_CTRL_2_FIRST_STARTUP_DONE_MASK       (uint8_t)(0x01U <<      \
                                       PMIC_RTC_CTRL_2_FIRST_STARTUP_DONE_SHIFT)
 
-#define PMIC_RTC_CTRL_2_STARTUP_DEST_MASK             (uint8_t)(0x03<<        \
+#define PMIC_RTC_CTRL_2_STARTUP_DEST_MASK             (uint8_t)(0x03U <<      \
                                             PMIC_RTC_CTRL_2_STARTUP_DEST_SHIFT)
 
-#define PMIC_RTC_CTRL_2_FAST_BIST_MASK                (uint8_t)(0x01<<        \
+#define PMIC_RTC_CTRL_2_FAST_BIST_MASK                (uint8_t)(0x01U <<      \
                                                PMIC_RTC_CTRL_2_FAST_BIST_SHIFT)
 
-#define PMIC_RTC_CTRL_2_LP_STANDBY_SEL_MASK           (uint8_t)(0x01<<        \
+#define PMIC_RTC_CTRL_2_LP_STANDBY_SEL_MASK           (uint8_t)(0x01U <<      \
                                           PMIC_RTC_CTRL_2_LP_STANDBY_SEL_SHIFT)
 
-#define PMIC_RTC_CTRL_2_XTAL_SEL_MASK                 (uint8_t)(0x03<<        \
+#define PMIC_RTC_CTRL_2_XTAL_SEL_MASK                 (uint8_t)(0x03U <<      \
                                                 PMIC_RTC_CTRL_2_XTAL_SEL_SHIFT)
 
-#define PMIC_RTC_CTRL_2_XTAL_EN_MASK                  (uint8_t)(0x01<<        \
+#define PMIC_RTC_CTRL_2_XTAL_EN_MASK                  (uint8_t)(0x01U <<      \
                                                  PMIC_RTC_CTRL_2_XTAL_EN_SHIFT)
 
 /*!
  * \brief   PMIC RTC Timer period bit mask
  */
-#define PMIC_RTC_INTERRUPTS_EVERY_MASK                (uint8_t)(0x03<<        \
+#define PMIC_RTC_INTERRUPTS_EVERY_MASK                (uint8_t)(0x03U <<      \
                                                PMIC_RTC_INTERRUPTS_EVERY_SHIFT)
 
 /*!
  * \brief   PMIC RTC Interrupt bit mask
  */
-#define PMIC_RTC_INTERRUPTS_IT_ALARM_MASK             (uint8_t)(0x01<<        \
+#define PMIC_RTC_INTERRUPTS_IT_ALARM_MASK             (uint8_t)(0x01U <<      \
                                             PMIC_RTC_INTERRUPTS_IT_ALARM_SHIFT)
 
-#define PMIC_RTC_INTERRUPTS_IT_TIMER_MASK             (uint8_t)(0x01<<        \
+#define PMIC_RTC_INTERRUPTS_IT_TIMER_MASK             (uint8_t)(0x01U <<      \
                                             PMIC_RTC_INTERRUPTS_IT_TIMER_SHIFT)
 
-#define PMIC_RTC_RESET_STATUS_RESET_STATUS_RTC_MASK   (uint8_t)(0x01<<        \
+#define PMIC_RTC_RESET_STATUS_RESET_STATUS_RTC_MASK   (uint8_t)(0x01U <<      \
                                   PMIC_RTC_RESET_STATUS_RESET_STATUS_RTC_SHIFT)
 
-#define PMIC_RTC_COMP_MSB_COMP_MSB_RTC_MASK           ((uint8_t)0xFF <<       \
+#define PMIC_RTC_COMP_MSB_COMP_MSB_RTC_MASK           (uint8_t)(0xFFU <<      \
                                   PMIC_RTC_COMP_MSB_COMP_MSB_RTC_SHIFT)
-#define PMIC_RTC_COMP_LSB_COMP_LSB_RTC_MASK           (0xFFU <<               \
+#define PMIC_RTC_COMP_LSB_COMP_LSB_RTC_MASK           (uint8_t)(0xFFU <<      \
                                   PMIC_RTC_COMP_LSB_COMP_LSB_RTC_SHIFT)
 /*==========================================================================*/
 /*                         Structures and Enums                             */
