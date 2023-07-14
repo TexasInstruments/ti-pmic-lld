c3b5b941ca41bcd76f84d76ededc4c6fd70d5ecb
Sai Ramakurthi <s-ramakurthi@ti.com>
2023-07-14 12:57:17 +0530

    [PDK-13184][PMIC][J784S4] Add pmic lib build support to J784S4
    - Since J784S4 is missing in board list and soc list, it is not getting
    packaged
    
    Signed-off-by: Sai Ramakurthi <s-ramakurthi@ti.com>

--- pmic_component.mk
+++ pmic_component.mk
@@ -66,11 +66,12 @@
 #
 ifeq ($(pmic_component_make_include), )
 
-drvpmic_BOARDLIST           = j721e_evm j7200_evm j721s2_evm
-drvpmic_SOCLIST             = j721e j7200 j721s2
+drvpmic_BOARDLIST           = j721e_evm j7200_evm j721s2_evm j784s4_evm
+drvpmic_SOCLIST             = j721e j7200 j721s2 j784s4
 drvpmic_j721e_CORELIST      = mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1 mpu1_0 c66xdsp_1 c66xdsp_2 c7x_1
 drvpmic_j7200_CORELIST      = mcu1_0 mcu1_1 mcu2_0 mcu2_1 mpu1_0
 drvpmic_j721s2_CORELIST     = mcu1_0
+drvpmic_j784s4_CORELIST     = mcu1_0
 drvpmic_app_j721e_CORELIST  = mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1
 drvpmic_app_j7200_CORELIST  = mcu1_0 mcu1_1 mcu2_0 mcu2_1
 drvpmic_fsm_mcu_app_CORELIST  = mcu1_0 mcu1_1
