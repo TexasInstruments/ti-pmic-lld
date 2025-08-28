3536024035cb3de95a3d136ce6456658589d7529
John Bui <j-bui1@ti.com>
2025-08-28 16:21:40 -0500

    PMICDRV-491: Implement IO Module
    Signed-off-by: John Bui <j-bui1@ti.com>

---
+++ include/pmic_io.h
@@ -0,0 +1,308 @@
+/******************************************************************************
+ * Copyright (c) 2025 Texas Instruments Incorporated - http://www.ti.com
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *****************************************************************************/
+#ifndef __PMIC_IO_H__
+#define __PMIC_IO_H__
+
+/**
+ * @file pmic_io.h
+ * @brief PMIC LLD serial communication I/O module.
+ */
+
+/**
+ * @defgroup DRV_PMIC_IO_MODULE PMIC Driver I/O Module
+ *
+ * @brief This module contains serial communication related functionalities,
+ * including reading and writing to/from the PMIC, as well as disabling/enabling
+ * communication CRC.
+ */
+
+/* ========================================================================== */
+/*                              Include Files                                 */
+/* ========================================================================== */
+
+#include "pmic_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ========================================================================== */
+/*                             Macros & Typedefs                              */
+/* ========================================================================== */
+
+/* ========================================================================== */
+/*                           Function Declarations                            */
+/* ========================================================================== */
+
+/**
+ * @brief Write a byte to the given PMIC `regAddr`, performing CRC on
+ * communications if necessary and enabled.
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param regAddr [IN] Register address to write to.
+ *
+ * @param txData [IN] Data to send to `regAddr`.
+ *
+ * @return PMIC_ST_SUCCESS if data was successfully transmitted, error code
+ * otherwise. For valid success/error codes, refer to @ref Pmic_ErrorCodes.
+ */
+int32_t Pmic_ioTxByte(const Pmic_Handle_t *handle, uint16_t regAddr, uint8_t txData);
+
+/**
+ * @brief Identical to Pmic_ioTxByte() in terms of functionality, but a critical
+ * section is started before the write. After the write, the critical section is
+ * stopped.
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param regAddr [IN] Register address to write to.
+ *
+ * @param txData [IN] Data to send to `regAddr`.
+ *
+ * @return PMIC_ST_SUCCESS if data was successfully transmitted, error code
+ * otherwise. For valid success/error codes, refer to @ref Pmic_ErrorCodes.
+ */
+int32_t Pmic_ioTxByte_CS(const Pmic_Handle_t *handle, uint16_t regAddr, uint8_t txData);
+
+/**
+ * @brief Read a byte from the given PMIC `regAddr`, extracting the desired
+ * register data from the CRC framed data returned by the PMIC.
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param regAddr [IN] Register address to read from.
+ *
+ * @param rxBuffer [OUT] Buffer to store result data in.
+ *
+ * @return PMIC_ST_SUCCESS if data was successfully obtained, error code
+ * otherwise. For valid success/error codes, refer to @ref Pmic_ErrorCodes.
+ */
+int32_t Pmic_ioRxByte(const Pmic_Handle_t *handle, uint16_t regAddr, uint8_t *rxData);
+
+/**
+ * @brief Identical to Pmic_ioRxByte() in terms of functionality, but a critical
+ * section is started before the read. After the read, the critical section is
+ * stopped.
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param regAddr [IN] Register address to read from.
+ *
+ * @param rxBuffer [OUT] Buffer to store result data in.
+ *
+ * @return PMIC_ST_SUCCESS if data was successfully obtained, error code
+ * otherwise. For valid success/error codes, refer to @ref Pmic_ErrorCodes.
+ */
+int32_t Pmic_ioRxByte_CS(const Pmic_Handle_t *handle, uint16_t regAddr, uint8_t *rxData);
+
+/**
+ * @brief Write up to 4 bytes to a linear sequence of registers starting at
+ * `baseAddr`, performing CRC on communications if necessary and enabled.
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param baseAddr [IN] Register address to start writing to.
+ *
+ * @param txData [IN] Data to send.
+ *
+ * @param count [IN] Number of bytes to write, must not be more than what can
+ * be stored in a uint32_t.
+ *
+ * @return PMIC_ST_SUCCESS if data was successfully transmitted, error code
+ * otherwise. For valid success/error codes, refer to @ref Pmic_ErrorCodes.
+ */
+int32_t Pmic_ioTxWordSeq(const Pmic_Handle_t *handle, uint16_t baseAddr, uint32_t txData, uint8_t count);
+
+/**
+ * @brief Read up to 4 bytes from a linear sequence of registers starting at
+ * `baseAddr`, extracting the desired register data from the CRC framed data
+ * returned by the PMIC.
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param baseAddr [IN] Register address to start reading from.
+ *
+ * @param rxData [OUT] Buffer to store result data in.
+ *
+ * @param count [IN] Number of bytes to read, must not be more than what can
+ * be stored in a uint32_t.
+ *
+ * @return PMIC_ST_SUCCESS if data was successfully obtained, error code
+ * otherwise. For valid success/error codes, refer to @ref Pmic_ErrorCodes.
+ */
+int32_t Pmic_ioRxWordSeq(const Pmic_Handle_t *handle, uint16_t baseAddr, uint32_t *rxData, uint8_t count);
+
+/**
+ * @brief Executes a read-modify-write routine such that the target bit field is
+ * modified without modifying other bit fields.
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param regAddr [IN] Target register address.
+ *
+ * @param shift [IN] Target bit field position.
+ *
+ * @param mask [IN] Target bit field mask.
+ *
+ * @param value [IN] Desired value to set the bit field to.
+ *
+ * @return Success code if read-modify-write operation was successful, error code
+ * otherwise. For valid success/error codes, refer to @ref Pmic_errorCodes.
+ */
+int32_t Pmic_ioReadModifyWrite(const Pmic_Handle_t *handle, uint8_t regAddr, uint8_t shift, uint8_t mask, uint8_t value);
+
+/**
+ * @brief Identical to Pmic_ioReadModifyWrite() API but starts a critical
+ * section before the read, modify, and write operations. Afterwards, the
+ * critical section is stopped.
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param regAddr [IN] Target register address.
+ *
+ * @param shift [IN] Target bit field position.
+ *
+ * @param mask [IN] Target bit field mask.
+ *
+ * @param value [IN] Desired value to set the bit field to.
+ *
+ * @return Success code if read-modify-write operation was successful, error code
+ * otherwise. For valid success/error codes, refer to @ref Pmic_errorCodes.
+ */
+int32_t Pmic_ioReadModifyWrite_CS(const Pmic_Handle_t *handle, uint8_t regAddr, uint8_t shift, uint8_t mask, uint8_t value);
+
+/**
+ * @brief Identical to Pmic_ioReadModifyWrite() API, but only the name of the
+ * bit field needs to be specified (case-sensitive).
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param regAddr [IN] Target register address.
+ *
+ * @param name [IN] Target bit field name.
+ *
+ * @param value [IN] Desired value to set the bit field to.
+ *
+ * @return Success code if read-modify-write operation was successful, error code
+ * otherwise. For valid success/error codes, refer to @ref Pmic_errorCodes.
+ */
+#define Pmic_ioReadModifyWriteByName(handle, regAddr, name, value) \
+    Pmic_ioReadModifyWrite((Pmic_Handle_t*)handle, (uint8_t)regAddr, (uint8_t)(name##_SHIFT), (uint8_t)(name##_MASK), (uint8_t)value)
+
+/**
+ * @brief Identical to Pmic_ioReadModifyWrite_CS() API, but only the name of the
+ * bit field needs to be specified (case-sensitive).
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param regAddr [IN] Target register address.
+ *
+ * @param name [IN] Target bit field name.
+ *
+ * @param value [IN] Desired value to set the bit field to.
+ *
+ * @return Success code if read-modify-write operation was successful, error code
+ * otherwise. For valid success/error codes, refer to @ref Pmic_errorCodes.
+ */
+#define Pmic_ioReadModifyWriteByName_CS(handle, regAddr, name, value) \
+    Pmic_ioReadModifyWrite_CS((Pmic_Handle_t*)handle, (uint8_t)regAddr, (uint8_t)(name##_SHIFT), (uint8_t)(name##_MASK), (uint8_t)value)
+
+/**
+ * @brief Modify a target bit field of width 1 without modifying other bit fields.
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param regAddr [IN] Target register address.
+ *
+ * @param shift [IN] Target bit field position.
+ *
+ * @param value [IN] Desired value to set the bit field to. If value is true,
+ * the bit field is set to 1. Otherwise, the bit field is set to 0.
+ *
+ * @return Success code if read-modify-write operation was successful, error code
+ * otherwise. For valid success/error codes, refer to @ref Pmic_errorCodes.
+ */
+int32_t Pmic_ioReadModifyWrite_b(const Pmic_Handle_t *handle, uint8_t regAddr, uint8_t shift, bool value);
+
+/**
+ * @brief Identical to Pmic_ioReadModifyWrite_b() API but starts a critical
+ * section before the read, modify, and write operations. Afterwards, the
+ * critical section is stopped.
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param regAddr [IN] Target register address.
+ *
+ * @param shift [IN] Target bit field position.
+ *
+ * @param value [IN] Desired value to set the bit field to. If value is true,
+ * the bit field is set to 1. Otherwise, the bit field is set to 0.
+ *
+ * @return Success code if read-modify-write operation was successful, error code
+ * otherwise. For valid success/error codes, refer to @ref Pmic_errorCodes.
+ */
+int32_t Pmic_ioReadModifyWrite_bCS(const Pmic_Handle_t *handle, uint8_t regAddr, uint8_t shift, bool value);
+
+/**
+ * @brief Get serial communication CRC enable/disable status.
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param isEnabled [OUT] Set to true (PMIC_ENABLE) if comms CRC is enabled,
+ * false (PMIC_DISABLE) if disabled. See @ref Pmic_EnableDisable.
+ *
+ * @return PMIC_ST_SUCCESS if serial communications CRC enable/disable status
+ * has been obtained, error code otherwise. For valid success/error codes, refer
+ * to @ref Pmic_ErrorCodes.
+ */
+int32_t Pmic_ioGetCrcEnableState(const Pmic_Handle_t *handle, bool *isEnabled);
+
+/**
+ * @brief Enable/disable serial communication CRC.
+ *
+ * @param handle [IN] PMIC interface handle.
+ *
+ * @param enable [IN] Set to true (PMIC_ENABLE) to enable comms CRC, false
+ * (PMIC_DISABLE) to disable. See @ref Pmic_EnableDisable.
+ *
+ * @return PMIC_ST_SUCCESS if serial communication CRC has been enabled or
+ * disabled, error code otherwise. For valid success/error codes, refer to
+ * @ref Pmic_ErrorCodes.
+ */
+int32_t Pmic_ioSetCrcEnableState(Pmic_Handle_t *handle, bool enable);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /*__PMIC_IO_H__*/
---
+++ src/pmic_io.c
@@ -0,0 +1,325 @@
+/******************************************************************************
+ * Copyright (c) 2025 Texas Instruments Incorporated - http://www.ti.com
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *****************************************************************************/
+/**
+ * @file pmic_io.c
+ *
+ * @brief This file contains LLD-Communication wrappers with CRC8 support for SPI.
+ */
+
+/* ========================================================================== */
+/*                              Include Files                                 */
+/* ========================================================================== */
+
+#include "pmic.h"
+
+#include "regmap/core.h"
+
+/* ========================================================================== */
+/*                            Macros & Typedefs                               */
+/* ========================================================================== */
+
+// Number of bytes to be transmitted/received.
+#define SPI_FRAME_LEN (4U)
+
+// Parts of a SPI frame - includes reading and writing
+#define SPI_FRAME_ADDR     (0U)
+#define SPI_FRAME_PAGE_RW  (1U)
+#define SPI_FRAME_STATUS_1 (0U)
+#define SPI_FRAME_STATUS_2 (1U)
+#define SPI_FRAME_WDATA    (2U)
+#define SPI_FRAME_WCRC     (3U)
+#define SPI_FRAME_RDATA    (2U)
+#define SPI_FRAME_RCRC     (3U)
+
+// Bit positions of SPI_FRAME_PAGE_RW
+#define SPI_BITPOS_RESERVED (0U)
+#define SPI_BITPOS_RW       (4U)
+#define SPI_BITPOS_PAGE     (5U)
+
+// Initial value for serial communication CRC.
+#define COMM_CRC_INITIAL_VALUE ((uint32_t)(0xFFU))
+
+/* ========================================================================== */
+/*                           Function Definitions                             */
+/* ========================================================================== */
+
+/**
+ *  Used CRC Polynomial:  x^8 + x^2 + x + 1
+ *   Evalution of CRC Polynomial value from equation:
+ *     1*x^8 + 0*x^7 + 0*x^6 + 0*x^5 + 0*x^4 + 0*x^3 + 1*x^2 + 1*x + 1
+ *  =  1     + 0     + 0     + 0     + 0     + 0     + 1     + 1   + 1
+ *  =  0x107(After discarding MSB bit) = 0x7
+ *
+ *  CRC Polynomial value: 0x07, Initial Value: 0x00, Final Value: 0x00
+ *  link to generate custom CRC table from polynomial:
+ *  http://www.sunshine2k.de/coding/javascript/crc/crc_js.html
+ */
+const static uint8_t CRC8_TABLE[] = {
+    0x00U, 0x07U, 0x0eU, 0x09U, 0x1cU, 0x1bU, 0x12U, 0x15U,
+    0x38U, 0x3fU, 0x36U, 0x31U, 0x24U, 0x23U, 0x2aU, 0x2dU,
+    0x70U, 0x77U, 0x7eU, 0x79U, 0x6cU, 0x6bU, 0x62U, 0x65U,
+    0x48U, 0x4fU, 0x46U, 0x41U, 0x54U, 0x53U, 0x5aU, 0x5dU,
+    0xe0U, 0xe7U, 0xeeU, 0xe9U, 0xfcU, 0xfbU, 0xf2U, 0xf5U,
+    0xd8U, 0xdfU, 0xd6U, 0xd1U, 0xc4U, 0xc3U, 0xcaU, 0xcdU,
+    0x90U, 0x97U, 0x9eU, 0x99U, 0x8cU, 0x8bU, 0x82U, 0x85U,
+    0xa8U, 0xafU, 0xa6U, 0xa1U, 0xb4U, 0xb3U, 0xbaU, 0xbdU,
+    0xc7U, 0xc0U, 0xc9U, 0xceU, 0xdbU, 0xdcU, 0xd5U, 0xd2U,
+    0xffU, 0xf8U, 0xf1U, 0xf6U, 0xe3U, 0xe4U, 0xedU, 0xeaU,
+    0xb7U, 0xb0U, 0xb9U, 0xbeU, 0xabU, 0xacU, 0xa5U, 0xa2U,
+    0x8fU, 0x88U, 0x81U, 0x86U, 0x93U, 0x94U, 0x9dU, 0x9aU,
+    0x27U, 0x20U, 0x29U, 0x2eU, 0x3bU, 0x3cU, 0x35U, 0x32U,
+    0x1fU, 0x18U, 0x11U, 0x16U, 0x03U, 0x04U, 0x0dU, 0x0aU,
+    0x57U, 0x50U, 0x59U, 0x5eU, 0x4bU, 0x4cU, 0x45U, 0x42U,
+    0x6fU, 0x68U, 0x61U, 0x66U, 0x73U, 0x74U, 0x7dU, 0x7aU,
+    0x89U, 0x8eU, 0x87U, 0x80U, 0x95U, 0x92U, 0x9bU, 0x9cU,
+    0xb1U, 0xb6U, 0xbfU, 0xb8U, 0xadU, 0xaaU, 0xa3U, 0xa4U,
+    0xf9U, 0xfeU, 0xf7U, 0xf0U, 0xe5U, 0xe2U, 0xebU, 0xecU,
+    0xc1U, 0xc6U, 0xcfU, 0xc8U, 0xddU, 0xdaU, 0xd3U, 0xd4U,
+    0x69U, 0x6eU, 0x67U, 0x60U, 0x75U, 0x72U, 0x7bU, 0x7cU,
+    0x51U, 0x56U, 0x5fU, 0x58U, 0x4dU, 0x4aU, 0x43U, 0x44U,
+    0x19U, 0x1eU, 0x17U, 0x10U, 0x05U, 0x02U, 0x0bU, 0x0cU,
+    0x21U, 0x26U, 0x2fU, 0x28U, 0x3dU, 0x3aU, 0x33U, 0x34U,
+    0x4eU, 0x49U, 0x40U, 0x47U, 0x52U, 0x55U, 0x5cU, 0x5bU,
+    0x76U, 0x71U, 0x78U, 0x7fU, 0x6aU, 0x6dU, 0x64U, 0x63U,
+    0x3eU, 0x39U, 0x30U, 0x37U, 0x22U, 0x25U, 0x2cU, 0x2bU,
+    0x06U, 0x01U, 0x08U, 0x0fU, 0x1aU, 0x1dU, 0x14U, 0x13U,
+    0xaeU, 0xa9U, 0xa0U, 0xa7U, 0xb2U, 0xb5U, 0xbcU, 0xbbU,
+    0x96U, 0x91U, 0x98U, 0x9fU, 0x8aU, 0x8dU, 0x84U, 0x83U,
+    0xdeU, 0xd9U, 0xd0U, 0xd7U, 0xc2U, 0xc5U, 0xccU, 0xcbU,
+    0xe6U, 0xe1U, 0xe8U, 0xefU, 0xfaU, 0xfdU, 0xf4U, 0xf3U
+};
+
+/**
+ * @brief Calculate SPI CRC byte.
+ *
+ * @details Initial value: 0xFF
+ * Big-endian bit stream order
+ *
+ * @param data [IN] Data for which CRC is to be determined.
+ *
+ * @param len [IN] Length of the buffer in bytes.
+ *
+ * @retval CRC value for data
+ */
+static uint8_t getCRC8Val(const uint8_t *data, uint8_t len) {
+    uint8_t crc = COMM_CRC_INITIAL_VALUE;
+
+    for (uint8_t i = 0U; i < len; i++) {
+        crc = CRC8_TABLE[data[i] ^ crc];
+    }
+
+    return crc;
+}
+
+int32_t Pmic_ioTxByte(const Pmic_Handle_t *handle, uint16_t regAddr, uint8_t txData) {
+    uint8_t frame[SPI_FRAME_LEN] = {0U};
+
+    int32_t status = Pmic_checkPmicHandle(handle);
+    if (status != PMIC_ST_SUCCESS) {
+        return status;
+    }
+
+    if (regAddr > UINT8_MAX) {
+        return PMIC_ST_ERR_INV_PARAM;
+    }
+
+    // Big-endian
+    frame[SPI_FRAME_ADDR] = regAddr;
+    frame[SPI_FRAME_PAGE_RW] = (0U << SPI_BITPOS_PAGE) | (0U << SPI_BITPOS_RW) | (0U << SPI_BITPOS_RESERVED);
+    frame[SPI_FRAME_WDATA] = txData;
+
+    if (handle->crcEnable) {
+        frame[SPI_FRAME_WCRC] = getCRC8Val(frame, 3U);
+    }
+
+    return handle->ioWrite(handle, regAddr, &frame[SPI_FRAME_WDATA], (handle->crcEnable) ? 2U : 1U);
+}
+
+int32_t Pmic_ioTxByte_CS(const Pmic_Handle_t *handle, uint16_t regAddr, uint8_t txData) {
+    int32_t status = PMIC_ST_SUCCESS;
+
+    Pmic_critSecStart(handle);
+    status = Pmic_ioTxByte(handle, regAddr, txData);
+    Pmic_critSecStop(handle);
+
+    return status;
+}
+
+int32_t Pmic_ioRxByte(const Pmic_Handle_t *handle, uint16_t regAddr, uint8_t *rxData) {
+    uint8_t frame[SPI_FRAME_LEN] = {0U};
+
+    int32_t status = Pmic_checkPmicHandle(handle);
+    if (status != PMIC_ST_SUCCESS) {
+        return status;
+    }
+
+    if (regAddr > UINT8_MAX) {
+        return PMIC_ST_ERR_INV_PARAM;
+    }
+
+    // Big-endian
+    frame[SPI_FRAME_ADDR] = regAddr;
+    frame[SPI_FRAME_PAGE_RW] = (0U << SPI_BITPOS_PAGE) | (1U << SPI_BITPOS_RW) | (0U << SPI_BITPOS_RESERVED);
+
+    status = handle->ioRead(handle, regAddr, &frame[SPI_FRAME_RDATA], (handle->crcEnable) ? 2U : 1U);
+    if (status != PMIC_ST_SUCCESS) {
+        return status;
+    }
+
+    if (handle->crcEnable & (getCRC8Val(frame, 3U) != frame[SPI_FRAME_RCRC])) {
+        return PMIC_ST_ERR_INV_COMM_CRC;
+    }
+
+    *rxData = frame[SPI_FRAME_RDATA];
+    return PMIC_ST_SUCCESS;
+}
+
+int32_t Pmic_ioRxByte_CS(const Pmic_Handle_t *handle, uint16_t regAddr, uint8_t *rxData) {
+    int32_t status = PMIC_ST_SUCCESS;
+
+    Pmic_critSecStart(handle);
+    status = Pmic_ioRxByte(handle, regAddr, rxData);
+    Pmic_critSecStop(handle);
+
+    return status;
+}
+
+int32_t Pmic_ioTxWordSeq(const Pmic_Handle_t *handle, uint16_t baseAddr, uint32_t txData, uint8_t count) {
+    int32_t status = PMIC_ST_SUCCESS;
+
+    if (count > sizeof(uint32_t)) {
+        status = PMIC_ST_ERR_INV_PARAM;
+    }
+
+    for (uint8_t i = 0; i < count; i++) {
+        status = Pmic_ioTxByte(handle, baseAddr + (uint16_t)i, (uint8_t)((txData >> (i * 8U)) & 0xFFU));
+        if (status != PMIC_ST_SUCCESS) {
+            return status;
+        }
+    }
+
+    return PMIC_ST_SUCCESS;
+}
+
+int32_t Pmic_ioRxWordSeq(const Pmic_Handle_t *handle, uint16_t baseAddr, uint32_t *rxData, uint8_t count) {
+    int32_t status = PMIC_ST_SUCCESS;
+    uint32_t value = 0U;
+    uint8_t regData = 0U;
+
+    if (rxData == NULL) {
+        return PMIC_ST_ERR_NULL_PARAM;
+    }
+
+    if (count > sizeof(uint32_t)) {
+        return PMIC_ST_ERR_INV_PARAM;
+    }
+
+    for (uint8_t i = 0U; i < count; i++) {
+        status = Pmic_ioRxByte(handle, baseAddr + (uint16_t)i, &regData);
+
+        if (status == PMIC_ST_SUCCESS) {
+            value |= (uint32_t)(regData << (i * 8U));
+        } else {
+            return status;
+        }
+    }
+
+    *rxData = value;
+    return PMIC_ST_SUCCESS;
+}
+
+int32_t Pmic_ioReadModifyWrite(const Pmic_Handle_t *handle, uint8_t regAddr, uint8_t shift, uint8_t mask, uint8_t value) {
+    uint8_t regData = 0U;
+
+    int32_t status = Pmic_ioRxByte(handle, regAddr, &regData);
+    if (status != PMIC_ST_SUCCESS) {
+        return status;
+    }
+
+    Pmic_setBitField(&regData, shift, mask, value);
+    return Pmic_ioTxByte(handle, regAddr, regData);
+}
+
+int32_t Pmic_ioReadModifyWrite_CS(const Pmic_Handle_t *handle, uint8_t regAddr, uint8_t shift, uint8_t mask, uint8_t value) {
+    int32_t status = PMIC_ST_SUCCESS;
+
+    Pmic_critSecStart(handle);
+    status = Pmic_ioReadModifyWrite(handle, regAddr, shift, mask, value);
+    Pmic_critSecStop(handle);
+
+    return status;
+}
+
+int32_t Pmic_ioReadModifyWrite_b(const Pmic_Handle_t *handle, uint8_t regAddr, uint8_t shift, bool value) {
+    return Pmic_ioReadModifyWrite(handle, regAddr, shift, 1U << shift, value ? 1U : 0U);
+}
+
+int32_t Pmic_ioReadModifyWrite_bCS(const Pmic_Handle_t *handle, uint8_t regAddr, uint8_t shift, bool value) {
+    int32_t status = PMIC_ST_SUCCESS;
+
+    Pmic_critSecStart(handle);
+    status = Pmic_ioReadModifyWrite_b(handle, regAddr, shift, value);
+    Pmic_critSecStop(handle);
+
+    return status;
+}
+
+int32_t Pmic_ioGetCrcEnableState(const Pmic_Handle_t *handle, bool *isEnabled) {
+    uint8_t regData = 0U;
+
+    int32_t status = Pmic_checkPmicHandle(handle);
+    if (status != PMIC_ST_SUCCESS) {
+        return status;
+    }
+
+    if (isEnabled == NULL) {
+        return PMIC_ST_ERR_NULL_PARAM;
+    }
+
+    status = Pmic_ioRxByte_CS(handle, INTERFACE_CONF_REGADDR, &regData);
+    if (status != PMIC_ST_SUCCESS) {
+        return status;
+    }
+
+    *isEnabled = Pmic_getBitField_b(regData, SPI_CRC_EN_SHIFT);
+    return PMIC_ST_SUCCESS;
+}
+
+int32_t Pmic_ioSetCrcEnableState(Pmic_Handle_t *handle, bool enable) {
+    int32_t status = Pmic_checkPmicHandle(handle);
+    if (status != PMIC_ST_SUCCESS) {
+        return status;
+    }
+
+    return Pmic_ioReadModifyWrite_bCS(handle, INTERFACE_CONF_REGADDR, SPI_CRC_EN_SHIFT, enable);
+}
