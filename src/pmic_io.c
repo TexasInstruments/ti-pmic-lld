82665b0e20583333631771a58566360bd6c7cbb6
John Bui <j-bui1@ti.com>
2025-10-09 14:02:49 -0500

    PMICDRV-532: Comms CRC is dropped on SPI Read Transactions
    Fix issue with Comms CRC not being included in SPI transactions when
    performing register reads.
    
    Authored-by: John Bui <j-bui1@ti.com>
    Signed-off-by: Michael Leonard <m-leonard@ti.com>

--- src/pmic_io.c
+++ src/pmic_io.c
@@ -49,50 +49,105 @@
 /*========================================================================== */
 /*                         Function Definitions                              */
 /*========================================================================== */
-static uint8_t IO_calcCRC8(uint8_t cmd, uint8_t rdwr, uint8_t dat) {
-    uint32_t crc = PMIC_COMM_CRC_INITIAL_VALUE;
-    uint32_t tmp = (uint32_t)((uint32_t)cmd << 16) | ((uint32_t)rdwr << 8) | (uint32_t)dat;
+/**
+ *  Used CRC Polynomial:  x^8 + x^2 + x + 1
+ *   Evaluation of CRC Polynomial value from equation:
+ *     1*x^8 + 0*x^7 + 0*x^6 + 0*x^5 + 0*x^4 + 0*x^3 + 1*x^2 + 1*x + 1
+ *  =  1     + 0     + 0     + 0     + 0     + 0     + 1     + 1   + 1
+ *  =  0x107(After discarding MSB bit) = 0x7
+ *
+ *  CRC Polynomial value: 0x07, Initial Value: 0x00, Final Value: 0x00
+ *  link to generate custom CRC table from polynomial:
+ *  http://www.sunshine2k.de/coding/javascript/crc/crc_js.html
+ */
+const static uint8_t CRC8_TABLE[] = {
+    0x00U, 0x07U, 0x0eU, 0x09U, 0x1cU, 0x1bU, 0x12U, 0x15U,
+    0x38U, 0x3fU, 0x36U, 0x31U, 0x24U, 0x23U, 0x2aU, 0x2dU,
+    0x70U, 0x77U, 0x7eU, 0x79U, 0x6cU, 0x6bU, 0x62U, 0x65U,
+    0x48U, 0x4fU, 0x46U, 0x41U, 0x54U, 0x53U, 0x5aU, 0x5dU,
+    0xe0U, 0xe7U, 0xeeU, 0xe9U, 0xfcU, 0xfbU, 0xf2U, 0xf5U,
+    0xd8U, 0xdfU, 0xd6U, 0xd1U, 0xc4U, 0xc3U, 0xcaU, 0xcdU,
+    0x90U, 0x97U, 0x9eU, 0x99U, 0x8cU, 0x8bU, 0x82U, 0x85U,
+    0xa8U, 0xafU, 0xa6U, 0xa1U, 0xb4U, 0xb3U, 0xbaU, 0xbdU,
+    0xc7U, 0xc0U, 0xc9U, 0xceU, 0xdbU, 0xdcU, 0xd5U, 0xd2U,
+    0xffU, 0xf8U, 0xf1U, 0xf6U, 0xe3U, 0xe4U, 0xedU, 0xeaU,
+    0xb7U, 0xb0U, 0xb9U, 0xbeU, 0xabU, 0xacU, 0xa5U, 0xa2U,
+    0x8fU, 0x88U, 0x81U, 0x86U, 0x93U, 0x94U, 0x9dU, 0x9aU,
+    0x27U, 0x20U, 0x29U, 0x2eU, 0x3bU, 0x3cU, 0x35U, 0x32U,
+    0x1fU, 0x18U, 0x11U, 0x16U, 0x03U, 0x04U, 0x0dU, 0x0aU,
+    0x57U, 0x50U, 0x59U, 0x5eU, 0x4bU, 0x4cU, 0x45U, 0x42U,
+    0x6fU, 0x68U, 0x61U, 0x66U, 0x73U, 0x74U, 0x7dU, 0x7aU,
+    0x89U, 0x8eU, 0x87U, 0x80U, 0x95U, 0x92U, 0x9bU, 0x9cU,
+    0xb1U, 0xb6U, 0xbfU, 0xb8U, 0xadU, 0xaaU, 0xa3U, 0xa4U,
+    0xf9U, 0xfeU, 0xf7U, 0xf0U, 0xe5U, 0xe2U, 0xebU, 0xecU,
+    0xc1U, 0xc6U, 0xcfU, 0xc8U, 0xddU, 0xdaU, 0xd3U, 0xd4U,
+    0x69U, 0x6eU, 0x67U, 0x60U, 0x75U, 0x72U, 0x7bU, 0x7cU,
+    0x51U, 0x56U, 0x5fU, 0x58U, 0x4dU, 0x4aU, 0x43U, 0x44U,
+    0x19U, 0x1eU, 0x17U, 0x10U, 0x05U, 0x02U, 0x0bU, 0x0cU,
+    0x21U, 0x26U, 0x2fU, 0x28U, 0x3dU, 0x3aU, 0x33U, 0x34U,
+    0x4eU, 0x49U, 0x40U, 0x47U, 0x52U, 0x55U, 0x5cU, 0x5bU,
+    0x76U, 0x71U, 0x78U, 0x7fU, 0x6aU, 0x6dU, 0x64U, 0x63U,
+    0x3eU, 0x39U, 0x30U, 0x37U, 0x22U, 0x25U, 0x2cU, 0x2bU,
+    0x06U, 0x01U, 0x08U, 0x0fU, 0x1aU, 0x1dU, 0x14U, 0x13U,
+    0xaeU, 0xa9U, 0xa0U, 0xa7U, 0xb2U, 0xb5U, 0xbcU, 0xbbU,
+    0x96U, 0x91U, 0x98U, 0x9fU, 0x8aU, 0x8dU, 0x84U, 0x83U,
+    0xdeU, 0xd9U, 0xd0U, 0xd7U, 0xc2U, 0xc5U, 0xccU, 0xcbU,
+    0xe6U, 0xe1U, 0xe8U, 0xefU, 0xfaU, 0xfdU, 0xf4U, 0xf3U
+};
 
-    /* Standard CRC-8 polynomial, X8 + X2 + X1 + 1, is used to calculate the
-     * checksum value based on the command and data which the MCU transmits to
-     * the TPS653850A-Q1 device. */
-    for (int8_t i = 0; i < 24; i++) {
-        uint64_t D = (uint64_t)((uint64_t)tmp & (uint64_t)0x800000) / (uint64_t)0x800000;
-        tmp = (tmp & (uint32_t)0x7FFFFF) * (uint32_t)2;
+/**
+ * @brief Helper function to get CRC8 data.
+ *
+ * @details Initial value: 0xFF
+ * Big-endian bit stream order
+ *
+ * @param data [IN] Data for which CRC is to be determined.
+ *
+ * @param length [IN] Length of the buffer in bytes.
+ *
+ * @return CRC value for data.
+ */
+static uint8_t getCRC8Val(const uint8_t *data, uint8_t len) {
+    uint8_t crc = PMIC_COMM_CRC_INITIAL_VALUE;
 
-        D = D ^ ((uint64_t)((uint64_t)crc & (uint64_t)0x80) / (uint64_t)0x80);
-        crc = (crc & 0x7FU) * 2U;
-
-        D *= (uint64_t)7;
-        crc ^= (uint32_t)D;
+    for (uint8_t i = 0U; i < len; i++) {
+        crc = CRC8_TABLE[data[i] ^ crc];
     }
 
-    /* Return the PMIC SPI MCRC value */
-    return (uint8_t)crc;
+    return crc;
 }
 
 int32_t Pmic_ioRxByte(Pmic_CoreHandle_t *handle, uint16_t regAddr, uint8_t *rxBuffer) {
     int32_t status = PMIC_ST_SUCCESS;
-    uint8_t rxBuf[PMIC_IO_BUF_SIZE] = {0};
+    uint8_t spiBuf[PMIC_IO_BUF_SIZE] = {0U};
+    uint8_t bufLen = 0U;
 
-    /*
-     *Frame 3 Bytes with IO header+data as per PMIC SPI IO algorithm
-     *explained in PMIC TRM
-     */
+    // spiBuf[0] = Target register address
+    spiBuf[0U] = (uint8_t)(regAddr & 0xFFU);
 
-    /*Set ADDR to pRxBuf[0], with ADDR[7:0] */
-    rxBuf[0U] = (uint8_t)(regAddr & 0xFFU);
+    // spiBuf[1] = page, R/W, reserved bits
+    // bits 7:5 -> page, bit 4 -> R/W, bit 3:0 -> reserved bits
+    spiBuf[1U] = (uint8_t)((uint8_t)((uint8_t)(regAddr >> 8U) & 0x7U) << 5U);
+    spiBuf[1U] |= PMIC_IO_REQ_RW;
+    bufLen = 3U;
 
-    /*Set PAGE to pRxBuf[1] 7:5 bits, with PAGE[2:0] */
-    /*Set R/W in pRxBuf[1] as bit-4, for read Request */
-    rxBuf[1U] = (uint8_t)((uint8_t)((uint8_t)(regAddr >> 8U) & 0x7U) << 5U);
-    rxBuf[1U] |= PMIC_IO_REQ_RW;
+    // spiBuf[3] = CCRC calculated based on spiBuf[0], spiBuf[1], and spiBuf[2]
+    spiBuf[3U] = getCRC8Val(spiBuf, bufLen);
+    bufLen = 4U;
 
-    status = handle->pFnPmicCommIoRead(handle, (uint8_t)PMIC_MAIN_INST, regAddr, &rxBuf[0], 4U);
+    // User-implemented hook transmits spiBuf then overwrites spiBuf with received data
+    status = handle->pFnPmicCommIoRead(handle, (uint8_t)PMIC_MAIN_INST, regAddr, spiBuf, bufLen);
 
+    // Validate PCRC
     if (status == PMIC_ST_SUCCESS) {
-        /*Copy data which shall be in rxBuf[2]/rxBuf[0] to rxBuffer */
-        *rxBuffer = rxBuf[2];
+        if (getCRC8Val(spiBuf, bufLen - 1U) != spiBuf[3U]) {
+            status = PMIC_ST_ERR_DATA_IO_CRC;
+        }
+    }
+
+    // Save RDATA[7:0]
+    if (status == PMIC_ST_SUCCESS) {
+        *rxBuffer = spiBuf[2U];
     }
 
     return status;
@@ -109,24 +164,30 @@ int32_t Pmic_ioRxByte_CS(Pmic_CoreHandle_t *handle, uint16_t regAddr, uint8_t *r
 }
 
 int32_t Pmic_ioTxByte(Pmic_CoreHandle_t *handle, uint16_t regAddr, uint8_t txData) {
-    /*Set write data to txBuf[2], with WDATA[7:0] */
-    uint8_t txBuf[PMIC_IO_BUF_SIZE] = {0U, 0U, txData, 0U};
+    int32_t status = PMIC_ST_SUCCESS;
+    uint8_t spiBuf[PMIC_IO_BUF_SIZE] = {0U};
+    uint8_t bufLen = 0U;
 
-    /* Frame 3 Bytes with IO header+data as per PMIC SPI IO algorithm explained
-     * in PMIC TRM */
+    // spiBuf[0] = Target register address
+    spiBuf[0U] = (uint8_t)(regAddr & 0xFFU);
 
-    /* Set ADDR to txbuf[0] with ADDR[7:0] */
-    txBuf[0U] = (uint8_t)(regAddr & 0xFFU);
+    // spiBuf[1] = page, R/W, reserved bits
+    // bits 7:5 -> page, bit 4 -> R/W, bit 3:0 -> reserved bits
+    spiBuf[1U] = (uint8_t)((uint8_t)((uint8_t)(regAddr >> 8U) & 0x7U) << 5U);
+    spiBuf[1U] &= (uint8_t)(~PMIC_IO_REQ_RW);
 
-    /*Set PAGE to txBuf[1] 7:5 bits with PAGE[2:0] */
-    /*Set R/W in txBuf[1] as bit-4, for Write Request */
-    txBuf[1U] = (uint8_t)((uint8_t)((uint8_t)(regAddr >> 8U) & 0x7U) << 5U);
-    txBuf[1U] &= (uint8_t)(~PMIC_IO_REQ_RW);
+    // spiBuf[2] = WDATA[7:0]
+    spiBuf[2U] = txData;
+    bufLen = 3U;
 
-    /*Set CRC data to txBuf[3], Bits 25-32 CRC */
-    txBuf[3U] = IO_calcCRC8(txBuf[0], txBuf[1], txData);
+    // spiBuf[3] = CCRC calculated based on spiBuf[0], spiBuf[1], and spiBuf[2]
+    spiBuf[3U] = getCRC8Val(spiBuf, bufLen);
+    bufLen = 4U;
 
-    return handle->pFnPmicCommIoWrite(handle, (uint8_t)PMIC_MAIN_INST, regAddr, &txBuf[0], 4U);
+    // User-implemented hook transmits spiBuf
+    status = handle->pFnPmicCommIoWrite(handle, (uint8_t)PMIC_MAIN_INST, regAddr, spiBuf, bufLen);
+
+    return status;
 }
 
 int32_t Pmic_ioTxByte_CS(Pmic_CoreHandle_t *handle, uint16_t regAddr, uint8_t txData) {
