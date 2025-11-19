63cf7da37b0bddc49f357b60306a110340faa417
Michael Leonard <m-leonard@ti.com>
2025-11-18 23:44:40 -0600

    feat(burton): implement core extended APIs

---
+++ devices/TPS6522x-Q1/src/pmic_core.c
@@ -0,0 +1,119 @@
+/******************************************************************************
+ * Copyright (c) 2025 Texas Instruments Incorporated - http://www.ti.com
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *****************************************************************************/
+/**
+ * @file pmic_core.c
+ *
+ * @brief PMIC LLD Core module source file for TPS6522x-Q1.
+ */
+#include "pmic.h"
+#include "pmic_core.h"
+#include "pmic_io.h"
+#include "regmap/core.h"
+
+#define PMIC_REG_LOCK_KEY   (0x00U)
+#define PMIC_REG_UNLOCK_KEY (0x5AU)
+
+int32_t Pmic_setRegLockState(Pmic_Handle_t *handle, bool lock)
+{
+    const uint8_t key = lock ? PMIC_REG_LOCK_KEY : PMIC_REG_UNLOCK_KEY;
+    int32_t status = Pmic_checkHandle(handle);
+
+    if (status == PMIC_ST_SUCCESS)
+    {
+        status = Pmic_ioTxByte_CS(handle, REGISTER_LOCK_REGADDR, key);
+    }
+
+    return status;
+}
+
+int32_t Pmic_getRegLockState(Pmic_Handle_t *handle, bool *isLocked)
+{
+    uint8_t regData = 0U;
+    int32_t status = Pmic_checkHandle(handle);
+
+    if ((status == PMIC_ST_SUCCESS) && (isLocked == NULL))
+    {
+        status = PMIC_ST_ERR_NULL_PARAM;
+    }
+
+    if (status == PMIC_ST_SUCCESS)
+    {
+        status = Pmic_ioRxByte_CS(handle, REGISTER_LOCK_REGADDR, &regData);
+    }
+
+    if (status == PMIC_ST_SUCCESS)
+    {
+        *isLocked = Pmic_getBitField_b(regData, REGISTER_LOCK_STATUS_SHIFT);
+    }
+
+    return status;
+}
+
+int32_t Pmic_setScratchPadValue(const Pmic_CoreHandle_t *pmicHandle, uint8_t scratchpadRegNum, uint8_t value)
+{
+    int32_t status = Pmic_checkHandle(pmicHandle);
+
+    if ((status == PMIC_ST_SUCCESS) && (scratchpadRegNum > PMIC_SCRATCH_PAD_REG_MAX))
+    {
+        status = PMIC_ST_ERR_INV_PARAM;
+    }
+
+    if (status == PMIC_ST_SUCCESS)
+    {
+        status = Pmic_ioTxByte_CS(pmicHandle, SCRATCH_PAD_REG_1_REGADDR + scratchpadRegNum, value);
+    }
+
+    return status;
+}
+
+int32_t Pmic_getScratchPadValue(const Pmic_CoreHandle_t *pmicHandle, uint8_t scratchpadRegNum, uint8_t *value)
+{
+    int32_t status = Pmic_checkHandle(pmicHandle);
+
+    if ((status == PMIC_ST_SUCCESS) && (scratchpadRegNum > PMIC_SCRATCH_PAD_REG_MAX))
+    {
+        status = PMIC_ST_ERR_INV_PARAM;
+    }
+
+    if ((status == PMIC_ST_SUCCESS) && (value == NULL))
+    {
+        status = PMIC_ST_ERR_NULL_PARAM;
+    }
+
+    if (status == PMIC_ST_SUCCESS)
+    {
+        status = Pmic_ioRxByte_CS(pmicHandle, SCRATCH_PAD_REG_1_REGADDR + scratchpadRegNum, value);
+    }
+
+    return status;
+}
